41e29a6b74f68e2f09fc9a6876527c99eb8817cc
metageek <francis@thibault.org>
2016-02-23 08:50:48 -0500

    Some improvements in the make system: .PHONY, clean:, lib:.

--- make.mk
+++ make.mk
@@ -1,3 +1,5 @@
+.PHONY:: all app clean depend libs lib tst check ro
+
 all::
 
 ifneq ($(strip $(subdirs)),)
@@ -5,9 +7,18 @@ all clean depend::
 	for dir in $(subdirs); do $(MAKE) -C $$dir $@ ; done
 endif
 
+clean::
+	$(RM) *.o *.a *~ $(app) depend.mk.bak
+
 INCLUDES:=-I$(depth)/include
 CFLAGS+=$(INCLUDES) -g -Wall -m32
 ASFLAGS+=$(INCLUDES) -m32
+
+ifneq ($(lib),)
+libs:: lib
+LIBFILE:=lib$(lib).a
+lib: $(LIBFILE)
+endif
 
 ifneq ($(LIBFILE),)
 $(LIBFILE): $(LIBOBJS)
